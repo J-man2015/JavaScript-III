015f12f5d07254e8c72d7a12524457ecc875148d
Jordan <j.egrey97@gmail.com>
2019-08-21 16:46:26 -0400

    completed

--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,37 @@
 // Principle 1
 
 // code example for Window Binding
-
+console.log(this);
 // Principle 2
 
 // code example for Implicit Binding
-
+const iceCream = {
+    flavor: "mint chocolate chip",
+    color: "green",
+    calories: 200,
+    info: function(){
+        return `this is ${this.flavor} and has ${this.calories} calories.`;
+    }
+}
+console.log(iceCream.info());
 // Principle 3
-
+function Newicecream(flavor,color,calories){
+    this.flavor= flavor;
+    this.color= color;
+    this.calories= calories;
+}
+let plum= new Newicecream("plum","purple",200);
+console.log(plum);
 // code example for New Binding
 
 // Principle 4
+function ghost(){
+    console.log(this.boo)
+}
+const myGhost = {
 
+    name: "casper",
+    boo: "boooo"
+}
+ghost.call(myGhost);
 // code example for Explicit Binding
